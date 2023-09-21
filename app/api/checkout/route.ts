3a67aa787cef96e9367a997ec9380e8da4629df1
bmo4401 <haiminh612a@gmail.com>
2023-09-21 23:59:51 +0700

    edit stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,45 +1,31 @@
 import stripe from '@/libs/stripe';
 import { NextResponse } from 'next/server';
+
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+
 export async function OPTIONS(req: Request, res: Response) {
   return NextResponse.json({}, { headers: corsHeaders });
 }
 export async function POST(req: Request, res: Response) {
-  if (req.method === 'POST') {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: 'price_1NskQnF8nm8tvbVtwq48ZguX',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: process.env.NEXTAUTH_URL,
-        cancel_url: process.env.NEXTAUTH_URL,
-      });
-      return NextResponse.json(
-        { url: session.url },
-        {
-          headers: corsHeaders,
-        },
-      );
-    } catch (err: any) {
-      return new NextResponse(err.message, { status: 400 });
-    }
-  } else {
-    return NextResponse.json('Method Not Allowed', {
-      status: 405,
-      headers: [['Allow', 'POST']],
-    });
-  }
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: 'price_1NskQnF8nm8tvbVtwq48ZguX',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: process.env.NEXTAUTH_URL,
+    cancel_url: process.env.NEXTAUTH_URL,
+  });
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    },
+  );
 }
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const signature = req.headers.get('stripe-signature') as string;
+  const signature = headers().get('Stripe-Signature') as string;
 
   let event: Stripe.Event;
 
@@ -14,17 +14,16 @@ export async function POST(req: Request) {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
-      process.env.STRIPE_WEBHOOK_SECRET,
+      process.env.STRIPE_WEBHOOK_SECRET!,
     );
   } catch (error: any) {
-    console.log('❄️ ~ file: route.ts:20 ~ error:', error);
     return new NextResponse(`Webhook Error: ${error.message}`, {
       status: 400,
     });
   }
 
+  const session = event.data.object as Stripe.Checkout.Session;
   if (event.type === 'checkout.session.completed') {
-    const session = event.data.object as Stripe.Checkout.Session;
     //@ts-ignore
     const email = session.billing_details?.email;
     await prisma.user.update({
